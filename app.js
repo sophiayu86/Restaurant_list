d2673573bc085e80e6afd649ae337d3fbc383f9f
sophiayu86 <sophiayu86@gmail.com>
2022-12-21 18:17:14 +0800

    Add detail page data from DB

--- app.js
+++ app.js
@@ -55,12 +55,13 @@ app.post('/restaurants', (req, res) => {
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
-app.get('/restaurants/:restaurant_id', (req, res) => {  
-    const restaurant = restaurantList.results.find(
-      restaurant => restaurant.id.toString() === req.params.restaurant_id
-    )  
-    res.render('show', { restaurant: restaurant })
-  })
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+})
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
