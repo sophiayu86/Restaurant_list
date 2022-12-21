1c825ed2b8e789cbfa22207cb5d094c3aa7c5560
sophiayu86 <sophiayu86@gmail.com>
2022-12-21 17:50:38 +0800

    Add create new restaurant

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose') // 載入 mongoose
 const Restaurant = require('./models/restaurant') // 載入 Restaurant model
+const bodyParser = require('body-parser')// 引用 body-parser
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -26,6 +27,9 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // routes setting
 app.get('/', (req, res) => {
   Restaurant.find() // 取出 model 裡的所有資料
@@ -33,7 +37,24 @@ app.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
-
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  const id = req.body.id
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const description = req.body.description
+  const rating = req.body.rating
+  return Restaurant.create({id,name,name_en,category,image,location,phone,google_map,rating,description})     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
 app.get('/restaurants/:restaurant_id', (req, res) => {  
     const restaurant = restaurantList.results.find(
       restaurant => restaurant.id.toString() === req.params.restaurant_id
