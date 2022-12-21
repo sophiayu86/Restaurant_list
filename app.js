beb8646324b4c3d07fb62791187fece0791ed153
sophiayu86 <sophiayu86@gmail.com>
2022-12-21 15:27:18 +0800

    Add mongoose

--- app.js
+++ app.js
@@ -2,10 +2,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-// require packages used in the project
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose') // 載入 mongoose
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
