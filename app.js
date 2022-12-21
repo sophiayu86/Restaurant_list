7ad022b7091ab7cebc86bdeaa8fd9638b623a4a3
sophiayu86 <sophiayu86@gmail.com>
2022-12-21 18:50:59 +0800

    feat: edit data in DB

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get('/restaurants/new', (req, res) => {
 })
 app.post('/restaurants', (req, res) => {
   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  const id = req.body.id
+  const restaurant_id = req.body.id
   const name_en = req.body.name_en
   const category = req.body.category
   const image = req.body.image
@@ -51,7 +51,7 @@ app.post('/restaurants', (req, res) => {
   const google_map = req.body.google_map
   const description = req.body.description
   const rating = req.body.rating
-  return Restaurant.create({id,name,name_en,category,image,location,phone,google_map,rating,description})     // 存入資料庫
+  return Restaurant.create({restaurant_id,name,name_en,category,image,location,phone,google_map,rating,description})     // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
@@ -62,7 +62,42 @@ app.get('/restaurants/:id', (req, res) => {
     .then((restaurant) => res.render('show', { restaurant }))
     .catch(error => console.log(error))
 })
-
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const restaurant_id = req.body.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const description = req.body.description
+  const rating = req.body.rating
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.id = restaurant_id
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.description = description
+      restaurant.rating = rating
+      return restaurant.save()
+    })
+    .then(()=> res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
