28544af530f132b42bc978174bb83971d14c82f7
sophiayu86 <sophiayu86@gmail.com>
2022-12-24 22:28:25 +0800

    fixed form required

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-// const mongoose = require('mongoose') // 載入 mongoose
 const bodyParser = require('body-parser')// 引用 body-parser
 const methodOverride = require('method-override') 
 const routes = require('./routes')
@@ -13,17 +12,6 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 require('./config/mongoose')
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-// // 取得資料庫連線狀態
-// const db = mongoose.connection
-// // 連線異常
-// db.on('error', () => {
-//   console.log('mongodb error!')
-// })
-// // 連線成功
-// db.once('open', () => {
-//   console.log('mongodb connected!')
-// })
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
